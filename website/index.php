473bfcae051b2079658ef851a3865d9eb3ad8964
pl03jlu <pl03jlu@eduvaud.ch>
2022-11-13 23:03:59 +0100

    GETS v0.0.1.7
    - Finished Login page visuals
    - Created temp database for storing user accounts
    - Created database connection
    - Website is working on MVC model
    - Created user data repository

---
+++ website/index.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * ETML
+ * @author      : Serghei Diulgherov
+ * Date         : 09.11.2022
+ * Description  : Ticket Management web app for "Gestion des élèves"
+ * 
+ * @copyright 2022 - ETML - Serghei Diulgherov
+ */
+
+$debug = false;
+
+if ($debug)
+{
+   error_reporting(E_ALL);
+   ini_set('disply_errors', '1');
+}
+
+date_default_timezone_set('Europe/Zurich');
+
+session_start();
+
+include_once 'controller/Controller.php';
+include_once 'controller/HomeController.php';
+
+class MainControler 
+{
+   /**
+    * This method finds what page is requested for load and forwards it to the necessary methods to build the view
+    * 
+    * @return void
+    */
+   public function dispatch()
+   {
+      if (!isset($_GET['controller']))
+      {
+         $_GET['controller'] = 'home';
+         $_GET['action'] = 'login';
+      }
+
+      $currentLink = $this -> menuSelected($_GET['controller']);
+      $this -> viewBuild($currentLink);
+   }
+
+   protected function menuSelected($page)
+   {
+      switch ($page)
+      {
+         case 'home':
+            $link = new HomeController();
+         break;
+      }
+      return $link;
+   }
+
+   protected function viewBuild($currentPage)
+   {
+      $content = $currentPage -> display();
+
+      include (dirname(__FILE__) . '/view/head.html');
+      //include (dirname(__FILE__) . '/view/header.html');
+      echo $content;
+      include (dirname(__FILE__) . '/view/footer.html');
+   }
+}
+
+$_controller = new MainControler();
+$_controller -> dispatch();
+
+?>
---
+++ website/view/page/home/login.php
@@ -0,0 +1,29 @@
+<!--
+
+    ETML
+    Author          : Serghei Diulgherov
+    Date            : 11.10.2022
+    Descriptiopn    : Login page
+
+-->
+
+<div class="login-page flex-center">
+    <div class="login-container">
+        <div class="login-left-area">
+            <div class="login-img-overlay">
+                <h3>Bienvenue sur</h3>
+                <h1>GETS!</h1>
+                <p>Bienvenue sur Geste Élèves Ticketing System</p>
+            </div>
+        </div>
+        <div class="login-right-area flex-center">
+            <h2>Log In</h2>
+            <form class="flex-center" id="login-form" method="post" action="index.php?controller=home&action=connect">
+                <input type="text" placeholder="Nom d'utilisateur" autocomplete="off" name="uname" required>
+                <input type="password" placeholder="Mot de Passe" name="psw" required>
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    </div>
+    <p>Copyright © Serghei Diulgherov - BnoppSoftware. All rights reserved.</p>
+</div>
